56e5a2da793f2f9e8d669956a4e18d86e329c65b
anita <anita.bacova@gmail.com>
2026-02-25 23:52:44 +0100

    py_kurz

--- 07 du_lesson_one.py
+++ 07 du_lesson_one.py
@@ -2,3 +2,4 @@
 # Popíšte algoritmus na uskutočnenie bankového prevodu a nakreslite flowchart procesu. Na
 # nákres použite aplikáciu draw.io. Ako výsledok spravte screenshot z aplikácie draw.io a pošlite do
 # MyStat ako obrázok.
+
