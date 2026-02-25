b653deb6488169dd37758d359874aec05644149e
anita <anita.bacova@gmail.com>
2026-02-25 23:52:22 +0100

    py_kurz

---
+++ 07 du_lesson_one.py
@@ -0,0 +1,4 @@
+# Úloha Algoritmus
+# Popíšte algoritmus na uskutočnenie bankového prevodu a nakreslite flowchart procesu. Na
+# nákres použite aplikáciu draw.io. Ako výsledok spravte screenshot z aplikácie draw.io a pošlite do
+# MyStat ako obrázok.
